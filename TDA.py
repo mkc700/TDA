608f520637a22f18adea06067750a216907c391a
Miguel Angel Hernandez Gonzalez <122412894+mkc700@users.noreply.github.com>
2025-02-04 21:39:50 -0600

    Update TDA.py
    oli

--- TDA.py
+++ TDA.py
@@ -1,13 +1,18 @@
 class Doctor:
-    def __init__(self, nombre, especialidad, numero_licencia):
+    # decidi no meter mas tda dentro de este objeto para no hacerlo mas complicado(numero de licencia)
+    # tambien procure no mencionar atributos que pudieran pertenecer facilmente a otras clases, ejemplo:
+    #  persona:  nombre,edad
+    #
+    #
+    def __init__(self, especialidad,experiencia,hospital,pacientes,horario_consulta):
         # Atributos del TDA
-        self._nombre = nombre
         self._especialidad = especialidad
-        self._numero_licencia = numero_licencia
+        self._experiencia = experiencia
+        self._hospital = hospital
+        self._pacientes = pacientes
+        self._horario_consulta = horario_consulta
 
-    def obtener_nombre(self):
-        """Devuelve el nombre del doctor"""
-        return self._nombre
+    # getters
 
     def obtener_especialidad(self):
         """Devuelve la especialidad del doctor"""
@@ -17,6 +22,25 @@ class Doctor:
         """Devuelve el número de licencia del doctor"""
         return self._numero_licencia
 
+    def obtener_experiencia(self):
+        """Devuelve los años de experiencia del doctor"""
+        return self._experiencia
+
+    def obtener_hospital(self):
+        """Devuelve el nombre del hospital donde trabaja el doctor"""
+        return self._hospital
+
+    def obtener_pacientes(self):
+        """Devuelve el numero de pacientes del doctor del doctor"""
+        return self._pacientes
+
+    def horario_consulta(self):
+        """Devuelve el horario de consulta del doctor"""
+        return self._horario_consulta
+
+
+    # Metodos
     def presentar_doctor(self):
         """Devuelve una representación en forma de cadena con la información del doctor"""
         return f"Doctor: {self._nombre}, Especialidad: {self._especialidad}, Licencia: {self._numero_licencia}"
+
